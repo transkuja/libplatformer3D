685638128da83473f06de6bd50936eda54ef9b41
Unknown <transkuja@hotmail.fr>
2017-10-24 17:22:05 +0200

    Load colliders

---
+++ Assets/LDChecker.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LDChecker : MonoBehaviour {
+
+    List<Collider> colliders;
+
+    void Start()
+    {
+        LoadColliders();
+    }
+
+    void LoadColliders()
+    {
+        Collider[] tmpColliders = FindObjectsOfType<Collider>();
+        colliders = new List<Collider>();
+
+        for (int i = 0; i < tmpColliders.Length; i++)
+        {
+            if (tmpColliders[i].isTrigger == false)
+            {
+                colliders.Add(tmpColliders[i]);
+                Debug.Log(tmpColliders[i]);
+            }
+        }
+        
+    }
+}

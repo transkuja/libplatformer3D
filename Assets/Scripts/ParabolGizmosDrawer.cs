2c8fbb357aab9279df28b9bbaee40df906f5ba37
Unknown <transkuja@hotmail.fr>
2017-11-06 20:58:27 +0100

    Fix direction and height but the parabol should start on the platform

--- Assets/Scripts/ParabolGizmosDrawer.cs
+++ Assets/Scripts/ParabolGizmosDrawer.cs
@@ -7,9 +7,9 @@ public static class ParabolGizmosDrawer
     [DrawGizmo(GizmoType.Active | GizmoType.Selected)]
     static void DrawParabolGizmos(GizmosDraw _from, GizmoType drawnGizmoType)
     {
-        float step = 0.1f;
-        if (_from.drawn)
-            return;
+        float step = 0.05f;
+        //if (_from.drawn)
+        //    return;
 
         Gizmos.color = Color.cyan;
         // Test
@@ -22,17 +22,18 @@ public static class ParabolGizmosDrawer
         {
             for (float i = 0; i < 20; i += step)
             {
-                Debug.Log("y = " + (LDChecker.Instance.maxParabolA * (i - (LDChecker.Instance.jumpRange / 2.0f)) * (i - (LDChecker.Instance.jumpRange / 2.0f)) + LDChecker.Instance.jumpHeight));
-                Gizmos.DrawLine(Vector3.up * (LDChecker.Instance.maxParabolA * Mathf.Pow(i - (LDChecker.Instance.jumpRange / 2.0f), 2) + LDChecker.Instance.jumpHeight) + direction * i,// + _from.transform.position,
-                    Vector3.up * (LDChecker.Instance.maxParabolA * Mathf.Pow((i + step) - (LDChecker.Instance.jumpRange / 2.0f), 2) + LDChecker.Instance.jumpHeight) + direction * (i + step));// + _from.transform.position);
+                //Debug.Log("x = " + i);
+                //Debug.Log("y = " + (LDChecker.Instance.maxParabolA * (i - (LDChecker.Instance.jumpRange / 2.0f)) * (i - (LDChecker.Instance.jumpRange / 2.0f)) + LDChecker.Instance.jumpHeight));
+                Gizmos.DrawLine(Vector3.up * (LDChecker.Instance.maxParabolA * Mathf.Pow(i - (LDChecker.Instance.jumpRange / 2.0f), 2) + LDChecker.Instance.jumpHeight) + direction * i + _from.transform.position,
+                    Vector3.up * (LDChecker.Instance.maxParabolA * Mathf.Pow((i + step) - (LDChecker.Instance.jumpRange / 2.0f), 2) + LDChecker.Instance.jumpHeight) + direction * (i + step) + _from.transform.position);
             }
         }
-        _from.drawn = true;
+        //_from.drawn = true;
     }
 
-    [DrawGizmo(GizmoType.NotInSelectionHierarchy)]
-    static void DrawParabolGizmosReset(GizmosDraw _from, GizmoType drawnGizmoType)
-    {
-        _from.drawn = false;
-    }
+    //[DrawGizmo(GizmoType.NotInSelectionHierarchy)]
+    //static void DrawParabolGizmosReset(GizmosDraw _from, GizmoType drawnGizmoType)
+    //{
+    //    _from.drawn = false;
+    //}
 }
